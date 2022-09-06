1f7bbe3bcac765a8cab6bdfa927d00434c0b361a
“Paola <priscila.criollo.est@tecazuay.edu.ec>
2022-09-06 07:43:00 -0500

    clase seguimiento

---
+++ src/main/java/com/example/prueba_paolac_williamt/model/Seguimiento.java
@@ -0,0 +1,26 @@
+package com.example.prueba_paolac_williamt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+@AllArgsConstructor
+@Data
+public class Seguimiento {
+    private boolean estado;
+    private String referencia;
+    private String prioridad;
+    private Date fecha_entrega;
+    private String tarea;
+    private String docente_responsable;
+    private String descripcion;
+    private String entregable;
+    private int ejecutado;
+    private String grupo;
+    private double costos_fijos;
+    private  int horas_estimadas;
+    private  int horas_actuales;
+    private String observacion;
+
+
+}
