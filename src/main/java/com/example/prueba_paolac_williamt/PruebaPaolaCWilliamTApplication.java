9665be3721c29566d45e2ea2e7dc0c429251260e
williamtocto <william.tocto.est@tecazuay.edu.ec>
2022-09-06 07:30:30 -0500

    Initial commit

---
+++ src/main/java/com/example/prueba_paolac_williamt/PruebaPaolaCWilliamTApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_paolac_williamt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPaolaCWilliamTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaPaolaCWilliamTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prueba_paolac_williamt/PruebaPaolaCWilliamTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_paolac_williamt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPaolaCWilliamTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
