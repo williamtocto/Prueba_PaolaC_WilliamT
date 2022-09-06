dd46ad0754dca4de17ab5b7baf7cb70fcc3ed763
williamtocto <william.tocto.est@tecazuay.edu.ec>
2022-09-06 08:08:39 -0500

    4

--- src/main/java/com/example/prueba_paolac_williamt/controller/SeguimientoController.java
+++ src/main/java/com/example/prueba_paolac_williamt/controller/SeguimientoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/producto")
+@RequestMapping("/api/seguimiento")
 public class SeguimientoController {
 
 
