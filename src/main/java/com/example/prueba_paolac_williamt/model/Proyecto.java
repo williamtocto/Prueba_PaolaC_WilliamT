1fef1a0fa085394281b453da17c21394e2a66417
williamtocto <william.tocto.est@tecazuay.edu.ec>
2022-09-06 07:40:59 -0500

    1

--- src/main/java/com/example/prueba_paolac_williamt/model/Proyecto.java
+++ src/main/java/com/example/prueba_paolac_williamt/model/Proyecto.java
@@ -1,4 +1,20 @@
 package com.example.prueba_paolac_williamt.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@Data
 public class Proyecto {
+    @Id
+    private String Referencia;
+    private String nombre_proyecto;
+    private String objetivo_estrategico;
+    private double objetivo_tactico;
+    private String estrategia;
+    private String objetivoOpertivo;
+
+
 }
